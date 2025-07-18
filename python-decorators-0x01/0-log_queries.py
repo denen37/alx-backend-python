dc753d8e2aafd757e2298d30c5f1caa04bee1b9f
Denen <awardenen@gmail.com>
2025-07-18 23:27:45 +0100

    Updated decorators

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,26 +1,18 @@
 import sqlite3
 import functools
 import logging
+from datetime import datetime
 
-# Setup a console logger
-def setup_console_logger():
-    logger = logging.getLogger("QueryLogger")
-    logger.setLevel(logging.INFO)
-    if not logger.handlers:
-        console_handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
-    return logger
 
-# Decorator to log SQL queries
 def log_queries(func):
     @functools.wraps(func)
-    def wrapper(query, *args, **kwargs):
-        logger = setup_console_logger()
-        logger.info(f"Query Executed: {query}")
-        return func(query, *args, **kwargs)
+    def wrapper(*args, **kwargs):
+        query = args[0]
+        logger = logging.getLogger("QueryLogger")
+        logger.info("{}: {}".format(datetime.now(), query))
+        return func(*args, **kwargs)
     return wrapper
+
 
 @log_queries
 def fetch_all_users(query):
