830bb13f1f40f7f3f441c0ae38a2041ca73f6b6c
Denen <awardenen@gmail.com>
2025-07-21 22:41:36 +0100

    Database Context Manager

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self):
+        self.db = 'C:/Users/HP/source/db/alx-airbnb.db'
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db)
+        print('Database connection opened')
+        return self.conn
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+        print('Database connection closed')
+
+def run_query(query):
+    with DatabaseConnection() as dbconn:
+        cursor = dbconn.cursor()
+        cursor.execute(query)
+        return cursor.fetchone()
